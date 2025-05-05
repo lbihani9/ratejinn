0a3d1748f526cc78d0cb8a91bf1a93d248d7f951
lbihani9 <lokeshbihani99@gmail.com>
2025-05-05 22:34:08 +0530

    Removes static adapter instance from RateJinnApplication

--- src/main/java/com/jinn/ratejinn/RatejinnApplication.java
+++ src/main/java/com/jinn/ratejinn/RatejinnApplication.java
@@ -1,8 +1,6 @@
 package com.jinn.ratejinn;
 
-import com.jinn.ratejinn.adapter.RedisAdapter;
 import com.jinn.ratejinn.config.RateJinnConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,9 +8,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(RateJinnConfig.class)
 public class RatejinnApplication {
-
-	@Autowired
-	private static RedisAdapter adapter;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatejinnApplication.class, args);
